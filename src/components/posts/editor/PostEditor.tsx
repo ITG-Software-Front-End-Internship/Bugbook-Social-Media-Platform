f250230efd368e19aa38e9a8e25a633bad469a1e
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-21 11:42:14 +0300

    ✨📝 feat(post-editor): complete client component with TipTap editor and server-side submission logic 🚀

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -1,5 +1,58 @@
+"use client";
+
+import { useSession } from "@/app/(main)/SessionProvider";
+import UserAvatar from "@/components/customComponents/UserAvatar";
+import { Button } from "@/components/ui/button";
+import PlaceHolder from "@tiptap/extension-placeholder";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import React from "react";
+import submitPost from "./actions";
+import "./styles.css";
 
 export default function PostEditor() {
-  return <div></div>;
+  const { user } = useSession();
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      PlaceHolder.configure({
+        placeholder: "What's going on?",
+      }),
+    ],
+  });
+
+  const input =
+    editor?.getText({
+      blockSeparator: "\n",
+    }) || "";
+
+  async function onSubmit() {
+    await submitPost(input);
+    editor?.commands.clearContent();
+  }
+
+  return (
+    <div className="bg-card flex flex-col gap-5 rounded-2xl p-5 shadow-sm">
+      <div className="flex items-center gap-5">
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="bg-background max-h-[20rem] w-full overflow-y-auto rounded-2xl px-5 py-3"
+        />
+        <div className="flex justify-end">
+          <Button
+            onClick={onSubmit}
+            disabled={!input.trim()}
+            className="min-w-20"
+          >
+            Post
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
