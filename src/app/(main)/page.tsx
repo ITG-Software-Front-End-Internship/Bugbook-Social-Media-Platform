f8e4457498d6c86ff33fbde8761076ec717959ef
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-22 00:40:32 +0300

    ✨ feat(posts): implement dynamic post feed using React Query

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,24 +1,13 @@
 import TrendsSidebar from "@/components/customComponents/TrendsSidebar";
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import ForYouFeed from "./ForYouFeed";
 
-export default async function Home() {
-  const posts = await prisma.post.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: postDataInclude,
-  });
-
+export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />;
-        })}
+        <ForYouFeed />
       </div>
       <TrendsSidebar />
     </main>
