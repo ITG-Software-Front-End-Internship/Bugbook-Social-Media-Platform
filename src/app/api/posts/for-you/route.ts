5563c7e0241689fca7a1a8d879d5b44b6a3a36b3
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-22 08:47:48 +0300

    🔁 refactor(feed): switch to React Infinite Query with "Load More" button

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,9 +1,14 @@
 import { cachedValidateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { postDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 10;
+
     const { user } = await cachedValidateRequest();
 
     if (!user) {
@@ -22,9 +27,19 @@ export async function GET() {
       orderBy: {
         createdAt: "desc",
       },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
     });
 
-    return Response.json(posts);
+    const hasMorePosts = posts.length > pageSize;
+    const nextCursor = hasMorePosts ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
   } catch (error) {
     console.error(error);
     return Response.json(
