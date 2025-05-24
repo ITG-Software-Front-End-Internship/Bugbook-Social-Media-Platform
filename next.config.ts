f888e7daeab47960f80d7fc5834877919ee44d34
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-24 14:38:49 +0300

    chore: allow remote image loading from uploadthing URLs in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
     },
   },
   serverExternalPackages: ["@node-rs/argon2"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/f/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+      },
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
