bf0cbd60e792a6a465cde25606323eb6bbc6dcce
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-28 00:29:22 +0300

    feat: implement server action for submitting comments with validation

---
+++ src/components/comments/actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { cachedValidateRequest } from "@/auth";
+import { validationsMessages } from "@/lib/constants";
+import prisma from "@/lib/prisma";
+import { getCommentDataInclude, PostData } from "@/lib/types";
+import { getCreateCommentSchema } from "@/lib/validations";
+import { getTranslations } from "next-intl/server";
+
+export async function submitComment({
+  post,
+  content,
+}: {
+  post: PostData;
+  content: string;
+}) {
+  const { user: loggedInUser } = await cachedValidateRequest();
+  if (!loggedInUser) {
+    throw new Error(`Unauthorized.`);
+  }
+
+  const t = await getTranslations();
+
+  const createCommentSchemaMessages = {
+    required: t(validationsMessages.required),
+  };
+
+  const { content: validatedContent } = getCreateCommentSchema(
+    createCommentSchemaMessages,
+  ).parse(content);
+
+  /**
+   * We need to create comment and return it to the front end.
+   * So we have to modify the cache when we post and delete a comment.
+   * Otherwise we have the same problem with revalidation taking so long if we have the too many pages
+   */
+
+  const newComment = await prisma.comment.create({
+    data: {
+      content: validatedContent,
+      postId: post.id,
+      userId: loggedInUser.id,
+    },
+    include: getCommentDataInclude(loggedInUser.id),
+  });
+
+  return newComment;
+}
