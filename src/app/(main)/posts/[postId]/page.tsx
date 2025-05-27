c5ccc265d1dd16298cc843dda3b4d7964f95197a
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-27 10:58:22 +0300

    👤 feat: add user info sidebar to post details page

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -1,10 +1,17 @@
 import { cachedValidateRequest } from "@/auth";
+import UserAvatar from "@/components/customComponents/UserAvatar";
+import FollowButton from "@/components/FollowButton";
+import Linkify from "@/components/Linkify";
 import Post from "@/components/posts/Post";
+import UserToolTip from "@/components/UserTooltip";
 import prisma from "@/lib/prisma";
-import { getPostDataInclude } from "@/lib/types";
+import { getPostDataInclude, UserData } from "@/lib/types";
+import { log } from "console";
+import { Loader2 } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-import { cache } from "react";
+import { cache, Suspense } from "react";
 
 interface PageParamsProps {
   postId: string;
@@ -73,6 +80,63 @@ export default async function Page({
       <div className="w-full min-w-0 space-y-5">
         <Post post={post} />
       </div>
+      <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none md:block lg:w-80">
+        <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+          <UserInfoSideBar user={post.user} />
+        </Suspense>
+      </div>
     </main>
+  );
+}
+
+interface UserInfoSideBarProps {
+  user: UserData;
+}
+
+async function UserInfoSideBar({ user }: UserInfoSideBarProps) {
+  const { user: loggedInUser } = await cachedValidateRequest();
+
+  if (!loggedInUser) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="mb-3 text-xl font-bold">About this user</div>
+      <UserToolTip user={user}>
+        <Link href={`/users/${user.username}`}>
+          <div className="flex items-center gap-3">
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+            <p className="line-clamp-1 break-all font-semibold hover:underline">
+              {user.displayName}
+            </p>
+            <p className="line-clamp-1 break-all text-primary-foreground">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
+      </UserToolTip>
+      {user.bio && (
+        <Linkify>
+          <div className="line-clamp-6 whitespace-pre-line break-words text-muted-foreground">
+            {user.bio}
+          </div>
+        </Linkify>
+      )}
+      {user.id !== loggedInUser.id && (
+        <FollowButton
+          userId={user.id}
+          initialState={{
+            followers: user._count.followers,
+            isFollowedByUser: user.followers.some(({ followerId }) => {
+              /** Current logged in user is following this user if it include 
+               the follower id
+               */
+              return followerId === loggedInUser.id;
+            }),
+          }}
+        />
+      )}
+    </div>
   );
 }
