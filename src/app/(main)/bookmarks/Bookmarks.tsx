48ef60d2738b184f6f03a9252422ecffaa8f884c
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-27 18:34:13 +0300

    📚 feat: display bookmarked posts with infinite scroll and loading states

---
+++ src/app/(main)/bookmarks/Bookmarks.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import InfiniteScrollContainer from "@/components/customComponents/InfiniteScrollContainer";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import kyInstance from "@/lib/ky";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+export default function Bookmarks() {
+  /**
+   * Notes: 
+    * For intial page we dont have an initialPage so we put it as null, 
+    and it should be as string infer or null 
+    so we add string | null to help typescript determine the type of pageParam in queryFn.
+   */
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "bookmarks"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/bookmarked",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        You do not have any bookmarks yet.
+      </p>
+    );
+  }
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while loading bookmarks
+      </p>
+    );
+  }
+
+  function handleOnBottomReached() {
+    if (hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }
+
+  return (
+    <InfiniteScrollContainer
+      onBottomReached={handleOnBottomReached}
+      className="space-y-5"
+    >
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+}
