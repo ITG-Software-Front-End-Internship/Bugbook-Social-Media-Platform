5600b0e35fbb005f9c656ad152ce0b7bc2831bc9
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-29 01:28:09 +0300

    🔔 feat: add API route to mark all notifications as read

---
+++ src/app/api/notifications/mark-as-read/route.ts
@@ -0,0 +1,49 @@
+import { cachedValidateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+
+/**
+ * PATCH: We want to update a value on a server
+ */
+
+export async function PATH() {
+  try {
+    const { user: loggedInUser } = await cachedValidateRequest();
+
+    if (!loggedInUser) {
+      return Response.json(
+        {
+          error: "Unauthorized",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    /**
+     * TODO: Im assuming that entering the notfication page marks all the notfication as read.
+     * TODO: We need to change it later to mark the notfication as read if we move to notfication detials page.
+     */
+    await prisma.notification.updateMany({
+      where: {
+        recipientId: loggedInUser.id,
+        isRead: false,
+      },
+      data: {
+        isRead: true,
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
