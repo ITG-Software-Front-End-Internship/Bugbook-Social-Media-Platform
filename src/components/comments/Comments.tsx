b9d7b7cc76b3289fb20c5e355490cf7623ce4868
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-05-28 12:24:02 +0300

    🔁 feat: add "load previous comments" functionality to post component

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,6 +1,7 @@
 import kyInstance from "@/lib/ky";
 import { CommentsPage, PostData } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { Button } from "../ui/button";
 import Comment from "./Comment";
 import CommentInput from "./CommentInput";
 
@@ -9,28 +10,30 @@ interface CommentsProps {
 }
 
 export default function Comments({ post }: CommentsProps) {
-  const { data, fetchNextPage, hasNextPage, isFetching, status } =
-    useInfiniteQuery({
-      queryKey: ["comments", post.id],
-      queryFn: ({ pageParam }) => {
-        return kyInstance
-          .get(`/api/posts/${post.id}/comments`)
-          .json<CommentsPage>();
-      },
-      /** Initally page params is null but we have to infer it to the type pageParam  */
-      initialPageParam: null as string | null,
-      /** Upward pagination */
-      getNextPageParam: (firstPage) => firstPage.previousCursor,
-      /** Select function to modify data before we then recive it up */
-      select: (data) => {
-        return {
-          /** Last comment at the top and the newest at the comment */
-          /** React query documnation */
-          pages: [...data.pages].reverse(),
-          pageParams: [...data.pageParams].reverse(),
-        };
-      },
-    });
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+    queryKey: ["comments", post.id],
+    queryFn: ({ pageParam }) => {
+      return kyInstance
+        .get(
+          `/api/posts/${post.id}/comments`,
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<CommentsPage>();
+    },
+    /** Initally page params is null but we have to infer it to the type pageParam  */
+    initialPageParam: null as string | null,
+    /** Upward pagination */
+    getNextPageParam: (firstPage) => firstPage.previousCursor,
+    /** Select function to modify data before we then recive it up */
+    select: (data) => {
+      return {
+        /** Last comment at the top and the newest at the comment */
+        /** React query documnation */
+        pages: [...data.pages].reverse(),
+        pageParams: [...data.pageParams].reverse(),
+      };
+    },
+  });
 
   /**          
           inifinite loading return type
@@ -51,6 +54,16 @@ export default function Comments({ post }: CommentsProps) {
   return (
     <div className="space-y-3">
       <CommentInput post={post} />
+      {hasNextPage && (
+        <Button
+          variant="link"
+          className="mx-auto block"
+          disabled={isFetching}
+          onClick={() => fetchNextPage()}
+        >
+          Load previous comments
+        </Button>
+      )}
       <div className="divide-y">
         {comments.map((comment) => {
           return <Comment key={comment.id} comment={comment} />;
