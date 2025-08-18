6971037f5381790bfe122310bae96424829597ef
Diaa Sharqawi <162372579+DiaaHSharqawi@users.noreply.github.com>
2025-08-19 01:04:52 +0300

    fix searrch page build time error

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,9 +1,11 @@
-import { Metadata } from "next";
-import { getTranslations } from "next-intl/server";
+"use server";
+
 import { cachedValidateRequest } from "@/auth";
 import TrendsSidebar from "@/components/customComponents/TrendsSidebar/TrendsSidebar";
-import SearchResults from "./components/SearchResults";
 import { searchTranslations } from "@/lib/translationKeys";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import SearchResults from "./components/SearchResults";
 
 export async function generateMetadata({
   searchParams,
@@ -12,7 +14,6 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const { q } = searchParams;
   const { user: loggedInUser } = await cachedValidateRequest();
-
   if (!loggedInUser || !q) return {};
   return {
     title: `Search results for ${q}`,
@@ -32,9 +33,7 @@ export default async function Page({
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
           <h1 className="line-clamp-2 break-all text-center text-2xl font-bold">
-            <bdi>
-              {t(searchTranslations.title)} {q ? `"${q}"` : ""}
-            </bdi>
+            <bdi>{t(searchTranslations.title)} {q ? `"${q}"` : ""}</bdi>
           </h1>
         </div>
         {q && <SearchResults query={q} />}
