f806e97a53ce657c358c0c1d74a76c774c1cff92
Diaa Sharqawi <162372579+DiaaHSharqawi@users.noreply.github.com>
2025-08-19 01:01:22 +0300

    fix : solve search page build time error

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,26 +1,29 @@
-"use server";
-
-import { cachedValidateRequest } from "@/auth";
-import TrendsSidebar from "@/components/customComponents/TrendsSidebar/TrendsSidebar";
-import { searchTranslations } from "@/lib/translationKeys";
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
+import { cachedValidateRequest } from "@/auth";
+import TrendsSidebar from "@/components/customComponents/TrendsSidebar/TrendsSidebar";
 import SearchResults from "./components/SearchResults";
+import { searchTranslations } from "@/lib/translationKeys";
 
-interface PageProps {
-  searchParams: { q?: string };
-}
-
-export async function generateMetadata({ searchParams }: PageProps): Promise<Metadata> {
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Record<string, string | undefined>;
+}): Promise<Metadata> {
   const { q } = searchParams;
   const { user: loggedInUser } = await cachedValidateRequest();
+
   if (!loggedInUser || !q) return {};
   return {
     title: `Search results for ${q}`,
   };
 }
 
-export default async function Page({ searchParams }: PageProps) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Record<string, string | undefined>;
+}) {
   const { q } = searchParams;
   const t = await getTranslations();
 
@@ -30,11 +33,11 @@ export default async function Page({ searchParams }: PageProps) {
         <div className="rounded-2xl bg-card p-5 shadow-sm">
           <h1 className="line-clamp-2 break-all text-center text-2xl font-bold">
             <bdi>
-              {t(searchTranslations.title)} &quot;{q}&quot;
+              {t(searchTranslations.title)} {q ? `"${q}"` : ""}
             </bdi>
           </h1>
         </div>
-        <SearchResults query={q} />
+        {q && <SearchResults query={q} />}
       </div>
       <TrendsSidebar />
     </main>
